15b65aa34c34feb6b49c5c76e6a165c5a0822f77
Mohammad Nadzif <nadzif.lambda@gmail.com>
2020-12-22 20:51:06 +0700

    string helper

--- src/helpers/StringHelper.php
+++ src/helpers/StringHelper.php
@@ -9,14 +9,16 @@
 namespace nadzif\base\helpers;
 
 
+use yii\helpers\StringHelper as YiiStringHelper;
+
 class StringHelper
 {
     public static function replace($string, $default, $array = [])
     {
         if ($string) {
             $placeholders = [];
-            foreach ((array) $array as $name => $value) {
-                if(is_array($value)){
+            foreach ((array)$array as $name => $value) {
+                if (is_array($value)) {
                     continue;
                 }
 
@@ -27,5 +29,43 @@ class StringHelper
         } else {
             return $default;
         }
+    }
+
+    public static function initial($string)
+    {
+        $explode = explode(' ', $string);
+
+        if (count($string) >= 2) {
+            if (count($explode) <= 1) {
+                return substr($explode[0], 0, 2);
+            } else {
+                $initialString = '';
+
+                $iC = 0;
+                foreach ($explode as $singleString) {
+                    if ($iC == 2) {
+                        break;
+                    }
+
+                    if (!$singleString) {
+                        continue;
+                    }
+
+                    $initialString .= substr($singleString, 0, 1);
+                    $iC++;
+                }
+
+                return $initialString;
+            }
+        } else {
+            return $string;
+        }
+    }
+
+    public static function shorten($string, $length = 30)
+    {
+        $regex = preg_replace('/&#?[a-z0-9]+;/i', '', preg_replace('/\r|\n/', '', $string));
+        $msg   = strip_tags($regex);
+        return YiiStringHelper::truncate($msg, $length);
     }
 }
